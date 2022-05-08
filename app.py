b5fc2fa55a84b61d170acaa0864a3287f20182de
Matheus Colombo <ccolombomatheus@gmail.com>
2022-05-09 00:58:25 +0100

    Fixed Functions and Structures

--- app.py
+++ app.py
@@ -11,12 +11,15 @@ from flask import (
 
 from werkzeug.utils import secure_filename
 import os
+import os.path
+import cv2
+from deepface import DeepFace
 
 app = Flask(__name__)
 
 app.secret_key = "super secret key"
 
-UPLOAD_FOLDER = "./imgs"
+UPLOAD_FOLDER = "./static/"
 ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -43,14 +46,23 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            return redirect(url_for("uploaded_file", filename=filename))
+            img = cv2.imread(os.path.join(UPLOAD_FOLDER, filename))
+            predictions = DeepFace.analyze(img, enforce_detection=False)
+            dominant_emotion = predictions["dominant_emotion"]
+            dominant_race = predictions["dominant_race"]
+            dominant_age = predictions["age"]
+            dominant_gender = predictions["gender"]
+            return render_template(
+                "results.html",
+                source=filename,
+                stats=predictions,
+                dominant_emotion=dominant_emotion,
+                dominant_race=dominant_race,
+                dominant_gender=dominant_gender,
+                dominant_age=dominant_age,
+            )
 
     return render_template("index.html")
-
-
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 if __name__ == "__main__":
